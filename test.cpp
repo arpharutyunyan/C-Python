b1e23623afde76c7ff25ad9f188396ce583d0fab
arpharutyunyan <arpine.harutyunyana@gmai.com>
2022-04-16 13:44:50 +0400

    first programm

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Hello world!"<<endl;
+    cout<<"This is written by Vim!!!"<<endl;
+    int a=1+2;
+    int b=2+3;
+    cout<<a<<endl<<b<<endl;
+
+    
+    return 0;
+}
