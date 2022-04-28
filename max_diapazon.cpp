eb48bef306da262e76abbb3919b529531915321e
arpharutyunyan <arpine.harutyunyana@gmai.com>
2022-04-28 22:44:00 +0400

    /usr/bin/env /bin/sh /tmp/Microsoft-MIEngine-Cmd-v2rs5cxg.reg

--- max_diapazon.cpp
+++ max_diapazon.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    int a[] = {10, 20, 30, 10, 20, 30, 40, 50, 60, 90};
+    int a[] = {10, 20, 30, 40, 50, 60, 10, 20, 30, 40, 50, 30, 90}; //100, 110, 120, 130, 140};
     int size = sizeof(a)/sizeof(a[0]);
     int len_diapazon = 1;
     int max = 0;
@@ -12,24 +12,48 @@ int main()
     
     for (int i = 0; i < size - 1; i++ ){
   
-        if (a[i] < a[i + 1] and i != size - 2){
+        if (a[i] < a[i + 1]){
 
             len_diapazon ++;
-            
+
+            if(len_diapazon > max){
+                
+                max = len_diapazon;
+                index = i + 1;
+                
+            }  
+
         }else{
             
-            if(len_diapazon > max){
-            
-                max = len_diapazon;
-                index = i;
-            }
-            
             len_diapazon = 1;
-        }
+        }  
         
     }
 
-    cout << max << endl;
+    cout << "max = " << max << endl;
+    cout << "index = " << index << endl;
+    int b[max];
 
+    
+
+    // for(int i = 0; i < max; i++){
+    //     b[i] = a[index - max + i + 1];
+    //     cout << b[i] << endl;
+    // }
+
+
+    // fill array from last index
+    for(int i = index; i > i - max; i--){
+
+        b[max - 1] = a[i];
+        max--;
+            
+    } 
+
+    for (int i: b){
+        cout << i << endl;
+    }
+
+    
     return 0;
 }
