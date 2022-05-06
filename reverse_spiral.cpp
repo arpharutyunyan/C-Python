161e13987a5a24d81fc78b94c64a5c9a50195c61
arpharutyunyan <arpine.harutyunyana@gmai.com>
2022-05-06 20:54:58 +0400

    Spiral

--- reverse_spiral.cpp
+++ reverse_spiral.cpp
@@ -13,28 +13,28 @@ int main(){
     cin >> col;
     int a[row][col];
 
-    int x = 1;
+    int x = row * col;
     int rowStart = 0;
     int rowEnd = col - 1;
     int colStart = 0;
     int colEnd = row - 1;
 
-    while(x <= row * col){
+    while(x > 0){
 
-        for (int i = rowStart; i <= rowEnd and x <= row * col; i++){
-            a[colStart][i] = x++;
+        for (int i = rowStart; i <= rowEnd and x > 0; i++){
+            a[colStart][i] = x--;
         }
 
-        for(int i = colStart + 1; i <= colEnd and row * col; i++){
-            a[i][rowEnd] = x++;
+        for(int i = colStart + 1; i <= colEnd and x > 0; i++){
+            a[i][rowEnd] = x--;
         }
 
-        for(int i = rowEnd - 1; i >= rowStart and row * col; i--){
-            a[colEnd][i] = x++;
+        for(int i = rowEnd - 1; i >= rowStart and x > 0; i--){
+            a[colEnd][i] = x--;
         }
 
-        for(int i = colEnd - 1; i > colStart and row * col; i--){
-            a[i][rowStart] = x++;
+        for(int i = colEnd - 1; i > colStart and x > 0; i--){
+            a[i][rowStart] = x--;
         }
 
         rowStart++;
