c6df6e82d856bdf93bd409a4a70e4a6bf05eef50
arpharutyunyan <arpine.harutyunyana@gmai.com>
2022-05-14 11:27:29 +0400

    sort with pointers

--- sort_pointer.cpp
+++ sort_pointer.cpp
@@ -14,15 +14,15 @@ void st_sort(int* pa, int len){
             int* b = pa + j + 1;
 
             if(*a > *b){
-                cout << "a " << a << "=" << *a << endl;
-                cout << "b " << b << "=" << *b << endl;
-                cout << "-------------------------" << endl;
+                // cout << "a " << a << "=" << *a << endl;
+                // cout << "b " << b << "=" << *b << endl;
+                // cout << "-------------------------" << endl;
                 int temp = *a;
                 *a = *b;
                 *b = temp;
-                cout << "a " << a << "=" << *a << endl;
-                cout << "b " << b << "=" << *b << endl;
-                cout << "++++++++++++++++++++++++++" << endl;
+                // cout << "a " << a << "=" << *a << endl;
+                // cout << "b " << b << "=" << *b << endl;
+                // cout << "++++++++++++++++++++++++++" << endl;
             }
         }
     }
@@ -35,27 +35,41 @@ void bubble_sort(int* pa, int len){
     cout << "bubble" << endl;
 
     for(int i = len - 1; i >= 0; i--){
-        for(int j = i - 1; j > len; j--){
+        for(int j = i - 1; j >= 0; j--){
            
-            int a = *(pa + i);
-            int b = *(pa + j);
-            if(a > b){
-                cout << "a " << &a << "=" << a << endl;
-                cout << "b " << &b << "=" << b << endl;
-                cout << "-------------------------" << endl;
-                int temp = a;
-                a = b;
-                b = temp;
-                cout << "a " << &a << "=" << a << endl;
-                cout << "b " << &b << "=" << b << endl;
-                cout << "++++++++++++++++++++++++++" << endl;
-
+            int* a = pa + j;
+            int* b = pa + i;
+            if(*a < *b){
+                int temp = *a;
+                *a = *b;
+                *b = temp;
             }
         }
     }
 
     print(pa, len);
 }
+
+void shaker_sort(int* pa, int len){
+
+    cout << "Shaker sort" << endl;
+
+    for(int i = 0; i < len; i++){
+        for(int j = 0; j < len - i; j++){
+           
+            int* a = pa + j;
+            int* b = pa + j + 1;
+            if(*a > *b){
+                int temp = *a;
+                *a = *b;
+                *b = temp;
+            }
+        }
+    }
+
+    print(pa, len);
+}
+
 
 void print(int* pa, int len){
 
@@ -66,15 +80,19 @@ void print(int* pa, int len){
     cout << endl << endl;
 }
 
+
 int main(){
 
     int a[] = {100, 90, 80, 70, 60, 50, 40, 30, 20, 10};
     int len = sizeof(a) / sizeof(a[0]);
+    cout << "Initial array" << endl;
     print(a, len);
 
     st_sort(a, len);
-    //print(a, len);
+
     bubble_sort(a, len);
+
+    shaker_sort(a, len);
 
 
     return 0;
