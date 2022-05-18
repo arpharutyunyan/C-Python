393deb0dc8b36a5f02e54ee7d057d9ae23abdd4a
arpharutyunyan <arpine.harutyunyana@gmai.com>
2022-05-19 00:13:20 +0400

    deque

--- dequeue.cpp
+++ dequeue.cpp
@@ -4,34 +4,52 @@ using namespace std;
 class Dequeue{
     private:
         int a[5];
-        int front = 1;
+        int front = 0;
         int end = 0;
-        int index_pop = 0;
-        int index_push = 0;
 
     public:
-        void push_front(int n){
-            a[--front] = n;
-        }
 
-        void push_back(int n){
-            a[end++] = n;
+    //---------LIFO-------------
+        void push_front(int n){
+            // if(front >= 0 and front < 5){
+            //     // a[front++] = n;
+            //     a[++front] = n;
+            // }
+
+            a[front++] = n;
         }
 
         int pop_front(){
-            return a[front++];
+            // if(front >= 0 and front < 5){
+            //     //a[front++] = n;
+            // }
+            return a[--front];
         }
 
         int pop_back(){
-            return a[end++];
+            // if(end < 0 and end > 5){
+            //     return;
+            // } 
+            return a[--end];
         }
 
+        
+
+    //-------------FIFO--------------
+        void push_back(int n){
+            // if(end >= 0 and end < 5){
+            //     a[end++] = n;
+            // }
+            a[end++] = n;
+        }
+
+    
         bool isEmpty(){
-            return index_push == 0;
+            return front == 0 and end == 0;
         }
 
         bool isFull(){
-            return index_push == 5;
+            return front > 0 or end > 0;
         }
 
 };
@@ -41,18 +59,28 @@ int main(){
     Dequeue dqu;
     cout << "full " << dqu.isFull() << endl;
     cout << "empty " << dqu.isEmpty() << endl;
-    // dqu.push(20);
-    // dqu.push(30);
-    // dqu.push(40);
-    // dqu.push(50);
-    // dqu.push(60);
+    // dqu.push_front(20);
+    // dqu.push_front(30);
+    // dqu.push_front(20);
+    // dqu.push_front(30);
+    // dqu.push_front(20);
+    dqu.push_back(40);
+    dqu.push_back(50);
+    dqu.push_back(40);
+    dqu.push_back(50);
+    dqu.push_back(40);
     cout << "full " << dqu.isFull() << endl;
     cout << "empty " << dqu.isEmpty() << endl;
-    // dqu.pop();
-    // dqu.pop();
-    // dqu.pop();
-    // dqu.pop();
-    // dqu.pop();
+    // dqu.pop_front();
+    // dqu.pop_front();
+    // dqu.pop_front();
+    // dqu.pop_front();
+    // dqu.pop_front();
+    dqu.pop_back();
+    dqu.pop_back();
+    dqu.pop_back();
+    dqu.pop_back();
+    dqu.pop_back();
     cout << "full " << dqu.isFull() << endl;
     cout << "empty " << dqu.isEmpty() << endl;
     
