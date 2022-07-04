771f266b44a1f5df22ab11997c947b17785e43ac
arpharutyunyan <arpine.harutyunyana@gmai.com>
2022-07-05 00:18:59 +0400

    file i/o

--- io_fstream.cpp
+++ io_fstream.cpp
@@ -6,27 +6,20 @@ using namespace std;
 
 int main(){
 
-    regex passport("([A][A-Z][0-9]{7})|([0][0-9]{8})");  // ID 
-    regex name_sname("\\b[A-Z][a-z]{3,}\\s+[A-Z][a-z]{5,}(yan)\\b");
+    string passport = "([A][A-Z][0-9]{7})|([0][0-9]{8})"; // ID 
+    string name_sname = "\\b[A-Z][a-z]{3,}\\s+[A-Z][a-z]{5,}(yan)\\b";
     string sdate = "([3][0-1]|[0]?[1-9]|[1-2][0-9])(\\.|\\/|\\-)([0]?[1-9]|[1][0-2])(\\.|\\/|\\-)([1-2]?[0-9]{2,3})";
     string date_reverse = "([1-2]?[0-9]{2,3})(\\.|\\/|\\-)([0]?[1-9]|[1][0-2])(\\.|\\/|\\-)([3][0-1]|[0]?[1-9]|[1-2][0-9])";
-    regex date(sdate + "|" + date_reverse);
-    regex ipv4("((([2][5][0-5])|([2][0-4][0-9])|([0-1]?[0-9]{1,2}))(\\.)?){4}"); 
-    //regex ipv6(""); 
+    string date = sdate + "|" + date_reverse;
+    string ipv4 = "((([2][5][0-5])|([2][0-4][0-9])|([0-1]?[0-9]{1,2}))\\.){3}(([2][5][0-5])|([2][0-4][0-9])|([0-1]?[0-9]{1,2}))";
 
-    smatch sm;
-
-    // string pass = "005491980";
-    // string name = "Sergey Ivanov";
-    // string d = "31-10-2022";
-    // string dr = "1931-10-31";
-    // string ipv_4 =  "255.255.255.255";
-    // string ipv_6 = "2001:0db8:11a3:09d7:1f34:8a2e:07a0:765d";
+    string regex_array[4] = {passport, name_sname, date, ipv4};
 
     string s[14] = {"005491980", "AS1234567", "00", "255.255.255.255", "255.239.1.85", "254.157.200.452", "Arpine Harutyunyan", 
                     "Anna Martirosyan", "Sergey Ivanov", "01.12.03", "31.12.2022", "31.12.3022", "2022/05/12", "1931.10.31"};
 
 
+    // writting checking phrases from array to source.txt
     ofstream ofs;
     ofs.open("./source.txt");
 
@@ -36,23 +29,32 @@ int main(){
 
     ofs.close();
 
+
+    // reading from source.txt file
     ifstream ifs;
     ifs.open("./source.txt");
 
+    //creating destination.tst file for saveing checked phrases
+    ofs.open("./destination.txt", ios::app);
+
     string phrase;
-    while (getline(ifs, phrase)){
-        ofs.open("./destination.txt", ios::app);
-        ofs << (phrase + "\n");
+    smatch sm;
+
+    while (getline(ifs, phrase)){ 
+
+        for(string rex : regex_array){
+
+            regex r(rex);
+
+            if(regex_match(phrase, sm, r)){
+                ofs << (phrase + "\n");
+            }
+        }    
     }
+
     ofs.close();
     ifs.close();
     
-    
-
-
-    
-    //cout << regex_match(dr, sm, date) << endl;
-
     return 0;
 }
 
